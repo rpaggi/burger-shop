da55c238c441975d5c5e8944a6147504c60f4ec8
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2016-06-07 13:36:14 -0300

    Login system finished

--- api/src/logins.js
+++ api/src/logins.js
@@ -1,24 +1,48 @@
 var crypto = require('crypto');
 
+var TokenGenerator = require( 'token-generator' )({
+        salt: 'bacon is life',
+        timestampMap: 'a1c2e3g4i5', // 10 chars array for obfuscation proposes 
+    });
+
+var updateToken = function(connection, id, token){
+	connection.query(
+		'UPDATE logins SET token = ? WHERE id = ?;',
+		[token, id],
+		function(err,result){
+			if(err) return err
+		});
+
+	return null;
+}
+
 exports.do = function(req, res) {
 	var login    = req.body["login"];
 	var password = req.body["password"];
 
 	if(login == null){
-		return res.status(400).json("Login em branco");
+		return res.status(400).json({"err":"Login em branco"});
 	}
 
 	if(password == null){
-		return res.status(400).json("Senha em branco");
+		return res.status(400).json({"err":"Senha em branco"});
 	}
 
 	cPassword = crypto.createHash('md5').update(password).digest("hex");
 
 	req.getConnection(function(err,connection){
-        connection.query('Select * from logins where login = ? and password = ?;',[login, cPassword],function(err,result){
+        connection.query('SELECT * FROM logins WHERE user = ? AND password = ?;',[login, cPassword],function(err,result){
 			if(err) return res.status(400).json(err);
 
-			return res.status(200).json(result);
+			if(Object.keys(result).length > 0){
+				var token = TokenGenerator.generate();
+				var ret   = updateToken(connection, result[0]['id'], token);
+
+				if(ret != null) return res.status(400).json(ret);
+				return res.status(200).json({"token":token});
+			}else return res.status(400).json({"err":"Login e senha inválidos"});
+				
+			
         });
     });
 }
