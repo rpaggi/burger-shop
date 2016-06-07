70d2b464c2e3b9da431b34be0349f03b0a588061
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2016-06-07 11:37:23 -0300

    Changes for advanced call

--- desktop/src/teste.cpp
+++ desktop/src/teste.cpp
@@ -4,11 +4,38 @@
 #include <string>
 
 int main(){
+	// initialize RestClient
 	RestClient::init();
 
-	//RestClient::Response r = RestClient::get("http://localhost:3000/profiles");
+	// get a connection object
+	RestClient::Connection* conn = new RestClient::Connection("http://localhost:3000");
 	
-	std::cout<< r.body <<std::endl;
+	// set connection timeout to 5s
+	conn->SetTimeout(10);
+	
+	// set headers
+	RestClient::HeaderFields headers;
+	headers["Accept"] = "application/json";
+	headers["Content-Type"] = "application/x-www-form-urlencoded";
+	headers["accept-encoding"] = "gzip, deflate";
+	headers["accept-language"] = "en-US,en;q=0.8";
+	conn->SetHeaders(headers)
+	
+	// append additional headers
+	conn->AppendHeader("token", "ronaldo")
+
+	
+	RestClient::Response r = conn->get("/valid-token")
+
+	std::cout << r.body << std::endl;
+	
+	// deinit RestClient. After calling this you have to call RestClient::init()
+	// again before you can use it
+	RestClient::disable();
+
+	//Simply mode working fine
+	//RestClient::Response r = RestClient::get("http://localhost:3000/profiles");
+	//std::cout<< r.body <<std::endl;
 
 	return 0;
 }
