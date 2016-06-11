60c0997730c09e9baec2d15f135d9707e94b393d
rpaggi <ronaldo.paggi@gmail.com>
2016-06-10 23:34:31 -0300

    Startin with Electron desktop app

---
+++ desktop/main.js
@@ -0,0 +1,34 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600, maximizable: false})
+
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+
+  mainWindow.maximize();
+  mainWindow.setMenu(null);
+
+  //mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
