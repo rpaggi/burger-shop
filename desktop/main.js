c5a51ace7393a2bc491d7954c2a8078cc81321b2
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2016-06-13 13:44:49 -0300

    Change for native electron menu

--- desktop/main.js
+++ desktop/main.js
@@ -1,18 +1,42 @@
 const electron = require('electron')
+const {Menu, MenuItem} = electron
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600, maximizable: false})
+  mainWindow = new BrowserWindow({
+    width: 1280, 
+    height: 720, 
+    icon: __dirname + '/ico/favicon-32x32.png'
+  })
 
   mainWindow.loadURL(`file://${__dirname}/src/index.html`)
 
-  mainWindow.maximize();
-  mainWindow.setMenu(null);
+  //menu.append(new MenuItem({type: 'separator'}));
+  const menu = new Menu();
+  menu.append(
+    new MenuItem({
+      label: 'Home',
+      submenu:[{
+        label: "Home1",
+        accelerator: "CmdOrCtrl+W",
+        role: "Home1",
+        click(){ app.quit(); }
+      },
+      {
+        label: "Home2",
+        accelerator: "F12",
+        click(){ mainWindow.webContents.openDevTools() }
+      },
+      {
+        label: "Home3"
+      }]
+    }
+  ));
 
-  //mainWindow.webContents.openDevTools()
+  mainWindow.setMenu(menu);
 
   mainWindow.on('closed', function () {
     mainWindow = null
@@ -30,5 +54,6 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
+    mainWindow.maximize();
   }
 })
