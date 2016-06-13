9ff0594bb038f852f3073841e91440eb61c66edb
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2016-06-13 15:41:00 -0300

    Menu test created and angular included

--- desktop/main.js
+++ desktop/main.js
@@ -5,10 +5,20 @@ const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
 
+function inArray(a, t){
+  for (var i = 0; i < a.length && a[i] !== t; i++) {
+
+  }
+  if(a[i] == null)
+    return false;
+  else
+    return true;
+}
+
 function createWindow () {
   mainWindow = new BrowserWindow({
-    width: 1280, 
-    height: 720, 
+    width: 1280,
+    height: 720,
     icon: __dirname + '/ico/favicon-32x32.png'
   })
 
@@ -20,21 +30,27 @@ function createWindow () {
     new MenuItem({
       label: 'Home',
       submenu:[{
-        label: "Home1",
-        accelerator: "CmdOrCtrl+W",
-        role: "Home1",
-        click(){ app.quit(); }
+        label: "Home1"
       },
       {
-        label: "Home2",
-        accelerator: "F12",
-        click(){ mainWindow.webContents.openDevTools() }
+        label: "Home2"
       },
       {
         label: "Home3"
       }]
     }
   ));
+  if(inArray(process.argv, "-debug"))
+    menu.append(
+      new MenuItem(
+        {label: 'Test',
+          submenu:[
+          {label: "Refresh", accelerator: "F5", role: "Home1", click(){ mainWindow.reload()}},
+          {label: "DevTools",accelerator: "F12",click(){ mainWindow.webContents.openDevTools()}},
+          {label: "&Quit", accelerator: "CmdOrCtrl+W", role: "Home1",click(){ app.quit();}}
+        ]
+      })
+    );
 
   mainWindow.setMenu(menu);
 
