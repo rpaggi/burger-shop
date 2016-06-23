75c2e42b848046f127db5cad7c84cda01746c6e3
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2016-06-23 10:24:35 -0300

    Restructuring files

---
+++ models/profiles.js
@@ -0,0 +1,26 @@
+exports.perm = function(req, res, next){
+	req.profiles = [1,2,3];
+	next();
+};
+
+exports.all = function(req, res) {
+	req.getConnection(function(err,connection){
+    connection.query('Select * from profiles;',[],function(err,result){
+			if(err) return res.status(400).json(err);
+
+			return res.status(200).json(result);
+    });
+  });
+}
+
+exports.get = function(req, res) {
+    var id = req.params.id;
+
+    req.getConnection(function(err,connection){
+			connection.query('Select * from profiles where id = ?;',[id],function(err,result){
+				if(err) return res.status(400).json(err);
+
+				return res.status(200).json(result);
+      });
+    });
+}
---
+++ models/tables.js
@@ -0,0 +1,92 @@
+var crypto = require('crypto');
+var TokenGenerator = getmodule('api_modules/token');
+
+exports.perm = function(req, res, next){
+	req.profiles = [1,2,3,4];
+	next();
+};
+
+exports.add = function(req, res){
+	TokenGenerator.verifiesPermission(req.get("token"), profiles, function(err){
+		if(err) return res.status(400).json(err);
+	});
+
+	var data = req.body;
+
+	if(isNaN(data.id) || data.id == 0){
+		return res.status(400).json({err:"Id inválido"});
+	}
+
+	if(data.name == null || data.name == ""){
+		return res.status(400).json({err:"Nome inválido"});
+	}
+
+	var data = req.body;
+
+	req.getConnection(function(err,connection){
+		connection.query('INSERT INTO tables SET ?;',[data],function(err,result){
+			if(err) return res.status(400).json(err);
+
+			return res.status(200).json(result);
+		});
+	});
+}
+
+exports.get = function(req, res){
+	TokenGenerator.verifiesPermission(req.get("token"), profiles, function(err){
+		if(err) return res.status(400).json(err);
+	});
+
+	var id = req.params.p;
+	req.getConnection(function(err, connection){
+		connection.query(
+			'SELECT * FROM tables WHERE id = ?',
+			[id],
+			function(err, result){
+				if (err) return res.status(400).json(err);
+
+				return res.status(200).json(result);
+			}
+		)
+	});
+}
+
+exports.delete = function(req, res){
+	TokenGenerator.verifiesPermission(req.get("token"), profiles, function(err){
+		if(err) return res.status(400).json(err);
+	});
+
+	var id = req.params.p;
+	req.getConnection(function(err, connection){
+		connection.query(
+			'DELETE FROM tables WHERE id = ?',
+			[id],
+			function(err, result){
+				if (err) return res.status(400).json(err);
+
+				return res.status(200).json(result);
+			}
+		)
+	});
+}
+
+exports.update = function(req, res){
+	TokenGenerator.verifiesPermission(req.get("token"), profiles, function(err){
+		if(err) return res.status(400).json(err);
+	});
+
+	var id = req.params.p;
+	var data = req.body;
+
+	req.getConnection(function(err, connection){
+		connection.query(
+			'UPDATE tables SET ? WHERE id = ?',
+			[data, id],
+			function(err, result){
+				if (err) return res.status(400).json(err);
+
+				return res.status(200).json(result);
+			}
+		)
+	});
+}
