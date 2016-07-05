f60bad60fe9d98049f9d5dba3407c05340bdf367
rpaggi <ronaldo.paggi@gmail.com>
2016-07-05 20:09:47 -0300

    Form to add product

--- src/controllers/app.js
+++ src/controllers/app.js
@@ -1,5 +1,6 @@
 var app = angular.module('app', []);
 app.controller('ApplicationController', ApplicationController);
+app.token = '5bbb1eggie145e1';
 
 function  ApplicationController(){
   var vm = this;
--- src/controllers/product.js
+++ src/controllers/product.js
@@ -2,4 +2,10 @@ app.controller('ProductController', ProductController);
 
 function  ProductController(){
   var vm = this;
+  vm.name = ""
+  vm.description = ""
+  vm.valueSell = 0.0;
+
+  vm.add = function(){
+  }
 }
