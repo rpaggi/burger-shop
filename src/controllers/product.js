24dbd5ef077722a7043c7fb7a8fafe449f48b442
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2016-07-06 08:40:10 -0300

    API communication working fine

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,18 +1,27 @@
 app.controller('ProductController', ['$http',function($http){
   var vm = this;
-  vm.name = ""
-  vm.description = ""
+  vm.name = "teste"
+  vm.description = "aaaa 111"
   vm.valueSell = 0.0;
 
-  vm.add = function(){
-    var data = {name:vm.name, description:vm.description, value_sell:vm.valueSell};
-    var config = {headers: {'token':app.token, 'data': data}};
+  $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
-    $http.post('http://localhost:3000/users', config)
+  vm.add = function(){
+    var data = "name="+vm.name+
+               "&description="+vm.description+
+               "&value_sell="+vm.valueSell.replace(/,/g, '.');
+
+    console.log(data);
+
+    var config = {headers: {'token':app.token}};
+
+    $http.post('http://localhost:3000/products', data, config)
     .then(function(response){
       console.log(response);
-    }), function(response){
-      console.log("error " + response);
-    };
+    }, function (response){
+      console.log("!!!error!!!");
+      console.log(JSON.stringify(response.data));
+      console.log("response status = " + response.status);
+    });
   }
 }]);
