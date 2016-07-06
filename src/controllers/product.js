14bcf3f5304875c545ab77758a6f5f11dd8dcd36
rpaggi <ronaldo.paggi@gmail.com>
2016-07-05 22:47:44 -0300

    Http call tests

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,11 +1,18 @@
-app.controller('ProductController', ProductController);
-
-function  ProductController(){
+app.controller('ProductController', ['$http',function($http){
   var vm = this;
   vm.name = ""
   vm.description = ""
   vm.valueSell = 0.0;
 
   vm.add = function(){
+    var data = {name:vm.name, description:vm.description, value_sell:vm.valueSell};
+    var config = {headers: {'token':app.token, 'data': data}};
+
+    $http.post('http://localhost:3000/users', config)
+    .then(function(response){
+      console.log(response);
+    }), function(response){
+      console.log("error " + response);
+    };
   }
-}
+}]);
